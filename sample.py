d3592d81d27045b4a19b328a320a998924aa4251
Nav Dessai <navdessai@Om-Mac.home>
2020-04-09 18:58:09 -0400

    adding picamera

--- sample.py
+++ sample.py
@@ -1,19 +1,34 @@
-import commands
+import cmdbroker
+import cmdmoves
 import json
 
 
 
-broker = commands.CommandBroker()
+broker = cmdbroker.CommandBroker()
 
 while True:
     print("Enter Command")
     x = int(input())
-    if x < 1 or x > 3:
+    if x == 0:
         break
     if x == 1:
-        broker.Send(commands.StartCommand())
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.MoveForward, 1, 1))
     elif x == 2:
-        broker.Send(commands.StopCommand())
-    else:
-        broker.Send(commands.MotionCommand(commands.MotionType.MoveForward, 1, 1))
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.MoveBackward, 1, 1))
+    elif x == 3:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.MoveRight, 1, 1))
+    elif x == 4:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.MoveLeft, 1, 1))
+    elif x == 5:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.MoveCenter, 1, 1))
+    elif x == 6:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.LookHCenter, 1, 1))
+    elif x == 7:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.LookLeft, 1, 1))
+    elif x == 8:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.LookRight, 1, 1))
+    elif x == 9:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.LookUp, 1, 1))
+    elif x == 10:
+        broker.Send(cmdmoves.MotionCommand(cmdmoves.MotionType.LookDown, 1, 1))
     
