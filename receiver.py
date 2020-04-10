d0c58117a37618806eb037f57edd0046564e0b4d
Nav Dessai <navdessai@Om-Mac.home>
2020-04-10 16:45:53 -0400

    moving start recording outside while

--- receiver.py
+++ receiver.py
@@ -21,10 +21,11 @@ def CameraRecording(name):
     # Accept a single connection and make a file-like object out of it
     connection = server_socket.accept()[0].makefile('wb')
     try:
-        while not stoprecording:
-            camera.start_recording(connection, format='h264')
-            camera.wait_recording(60)
+        camera.start_recording(connection, format='h264')
             
+        while not stoprecording:
+            camera.wait_recording(60)
+
         camera.stop_recording()
     finally:
         connection.close()
